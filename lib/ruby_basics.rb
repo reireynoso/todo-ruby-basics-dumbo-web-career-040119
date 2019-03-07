12fba536d62f492d5b567053fa562d69688999bd
Reinald Reynoso <reireynoso@gmail.com>
2019-03-07 18:57:32 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
 def argue
+  return  
+end
+
+def greeting(greeting,name)
   
 end
 
-def greeting
-
+def return_a_value(phrase)
+  return phrase
 end
 
-def return_a_value
-
+def last_evaluated_value(phrase)
+  return phrase
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(phrase = "cheese")
+  return phrase
 end
